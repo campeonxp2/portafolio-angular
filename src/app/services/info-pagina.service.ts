9ca2d9288d4bcd95e207f5b372387a91c06c1e8d
Enrique Valdivia <enriquevaldiviaoutlook.com>
2018-12-20 17:41:35 -0500

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,33 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) {
 
     // console.log('Servicio de InfoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
 
+  }
+
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
+      });
+
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-aae5f.firebaseio.com/equipo.json')
+      .subscribe((resp: any[])  => {
+
+        this.equipo = resp;
       });
   }
+
 }
